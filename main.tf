8fd18b11a5474e165da88d1ad6af0e4f9f6e7237
Donna-Smee <dailydosesofdonuts@gmail.com>
2025-02-07 07:24:25 -0500

    fix: replaced curly quotes with straight quotes, fixed formatting, changed region to Canada Central, init.sh config is set to base64_encode = true, added os_disk configs to VM definition, changed image_reference to source_image_reference. Changed applied for a working terraform

--- main.tf
+++ main.tf
@@ -18,7 +18,11 @@ terraform {
 # Define providers and their config params
 provider "azurerm" {
   # Leave the features block empty to accept all defaults
-  features {}
+  features {
+    resource_group {
+        prevent_deletion_if_contains_resources = false
+   }
+  }
 }
 
 provider "cloudinit" {
@@ -26,51 +30,51 @@ provider "cloudinit" {
 }
 
 # Define labelPrefix variable
-variable “labelPrefix” {
-	type = string
-	description = “Your college username. This will form the beginning of various resource names.”
+variable "labelPrefix" {
+  type        = string
+  description = "Your college username. This will form the beginning of various resource names."
 }
 
 # Define region variable
-variable “region” {
-	default = “westus3”
+variable "region" {
+  default = "Canada Central"
 }
 
 # Define the admin_username variable
-variable “admin_username” {
-	type = string
-	default = “azureadmin”
-	description = “The username for the local user account on the VM.”
+variable "admin_username" {
+  type        = string
+  default     = "azureadmin"
+  description = "The username for the local user account on the VM."
 }
 
 # define the resource group
-resource “azurerm_resource_group” “rg” {
-	name = "${var.labelPrefix}-A05-RG"
-	location = var.region
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.labelPrefix}-A05-RG"
+  location = var.region
 }
 
 # define the public ip
 resource "azurerm_public_ip" "webserver" {
-  name                = “${var.labelPrefix}A05PublicIP”
+  name                = "${var.labelPrefix}A05PublicIP"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
 }
 
 # define the virtual network
-resource “azurerm_virtual_network” “vnet” {
-	name = “${var.labelPrefix}A05Vnet”
-    address_space = [“10.0.0.0/16”]
-    location = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.labelPrefix}A05Vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 # define the subnet
-resource “azurerm_subnet” “webserver” {
-name = “${var.labelPrefix}A05Subnet”
-resource_group_name =azurerm_resource_group.rg.name
-virtual_network_name = “azurerm_virtual_network.vnet.name”
-address_prefixes = [“10.0.1.0/24”]
+resource "azurerm_subnet" "webserver" {
+  name                  = "${var.labelPrefix}A05Subnet"
+  resource_group_name   = azurerm_resource_group.rg.name
+  virtual_network_name  = azurerm_virtual_network.vnet.name
+  address_prefixes      = ["10.0.1.0/24"]
 }
 
 # define the security group for the web server
@@ -104,7 +108,6 @@ resource "azurerm_network_security_group" "sg" {
   }
 }
 
-
 # define the Virtual Network Interface Card (NIC)
 resource "azurerm_network_interface" "nic" {
   name                = "${var.labelPrefix}A05NIC"
@@ -125,11 +128,10 @@ resource "azurerm_network_interface_security_group_association" "nsg_association
   network_security_group_id = azurerm_network_security_group.sg.id
 }
 
-
 # config the init script for the VM
 data "cloudinit_config" "web_server" {
   gzip          = false
-  base64_encode = false
+  base64_encode = true
 
   part {
     filename     = "init.sh"
@@ -144,14 +146,13 @@ resource "azurerm_linux_virtual_machine" "web_server" {
   name                = "${var.labelPrefix}A05VM"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"  
+  size                = "Standard_B1s"
   admin_username      = var.admin_username
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file("${path.module}/public_key.pub") 
-}
+    public_key = file("${path.module}/public_key.pub")
+  }
 
-  
   network_interface_ids = [
     azurerm_network_interface.nic.id  # Link the NIC to this VM
   ]
@@ -159,11 +160,15 @@ resource "azurerm_linux_virtual_machine" "web_server" {
   # Use Cloud-init to run the init.sh script on first boot
   custom_data = data.cloudinit_config.web_server.rendered
 
-  # Define the image for the VM (Ubuntu)
-  image_reference {
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
     version   = "latest"
   }
 
