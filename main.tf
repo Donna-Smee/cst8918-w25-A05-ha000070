b9acad9d65c33bc20585e8b063a26ac681d3285b
Donna-Smee <dailydosesofdonuts@gmail.com>
2025-02-07 05:09:56 -0500

    feat: define the security group with security rules: SSH, HTTP

--- main.tf
+++ main.tf
@@ -70,5 +70,36 @@ resource “azurerm_subnet” “webserver” {
 name = “${var.labelPrefix}A05Subnet”
 resource_group_name =azurerm_resource_group.rg.name
 virtual_network_name = “azurerm_virtual_network.vnet.name”
-address-prefixes = [“10.0.1.0/24”]
+address_prefixes = [“10.0.1.0/24”]
+}
+
+# define the security group for the web server
+resource "azurerm_network_security_group" "sg" {
+  name                = "${var.labelPrefix}A05SecurityGroup"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
